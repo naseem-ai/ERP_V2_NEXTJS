097d1894a6910d6a91341c8947d4406264ef9d20
naseem-ai <naseem.sid.bpl@gmail.com>
2025-03-14 10:57:18 +0530

    new

--- .next/static/chunks/src_ca58c909._.js
+++ .next/static/chunks/src_ca58c909._.js
@@ -1218,13 +1218,13 @@ function Header() {
                             }, this),
                             " ",
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("b", {
-                                children: "Color"
+                                children: "ERP"
                             }, void 0, false, {
                                 fileName: "[project]/src/components/header/header.tsx",
                                 lineNumber: 53,
                                 columnNumber: 49
                             }, this),
-                            " Admin"
+                            " V2"
                         ]
                     }, void 0, true, {
                         fileName: "[project]/src/components/header/header.tsx",
